dde96073500683d0076fb15ad41b689b2f26c46d
Tyler Teschon <tbtesch@gmail.com>
2020-04-12 15:14:58 -0400

    Create dictionary_setdefault.py
    Function that combines two dictionaries into one, with each key listed only once, and the values from dictionary 1 taking precedence, if a key is included in both dictionaries.

---
+++ dictionary_setdefault.py
@@ -0,0 +1,11 @@
+def combine_guests(guests1, guests2):
+  # Combine both dictionaries into one, with each key listed 
+  # only once, and the value from guests1 taking precedence
+  for guest, party in guests2.items():
+    guests1.setdefault(guest, party)
+  return guests1
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
